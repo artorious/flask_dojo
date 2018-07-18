730bc25c2e06d3b988415b86fed333837941472d
Arthur Ngondo <ngondoarthur@gmail.com>
2018-07-18 07:28:32 +0300

    Test Authentification - User login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -16,7 +16,10 @@ def test_register(client, app):
         <headers> will have a <Location> header with the login URL when the 
         register view redirects to the login view.
         <data> contains the body of the response as bytes
-
+        
+        pytest.mark.parametrize tells Pytest to run the same test function 
+        with different arguments.
+        Tests different invalid input and error messages
     """
     assert client.get('/auth/register').status_code == 200
     response = client.post(
@@ -30,7 +33,7 @@ def test_register(client, app):
         ).fetchone() is not None
 
 
-@pytest.mark.parametriz(('username', 'password', 'message'), (
+@pytest.mark.parametrize(('username', 'password', 'message'), (
     ('', '', b'Username is required.'),
     ('a', '', b'Password is required.'),
     ('test', 'test', b'already registered'),
@@ -42,4 +45,43 @@ def test_register_validate_input(client, username, password, message):
         data={'username': username, 'password': password}
     )
     assert message in response.data
+
+
+def test_login(client, auth):
+    """ Tests user login
+
+        Make a GET request and return the Response object returned by Flask.
+        To test that a page renders successfully, a simple request is made
+        and checked for a 200 OK status_code. 500 Internal Server Error code
+        is returned on failure.
+        
+        Rather than testing the data in the database, session should have
+        user_id set ater logging in.
+
+        Using <client> in a <with> block allows accessing context variables
+        such as session after the response is returned.
+
+        pytest.mark.parametrize tells Pytest to run the same test function 
+        with different arguments.
+        Tests different invalid input and error messages
+    """
+    assert client.get('/auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers['Location'] == 'http://localhost/'
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.'),
+))
+
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
+
 
